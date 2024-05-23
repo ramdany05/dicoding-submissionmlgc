1653592fe606fef36b8c114e202f78ec0e207c3c
Ramdany Suhandi <116938810+ramdany05@users.noreply.github.com>
2024-05-23 16:28:59 +0700

    update url loadModel

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,7 @@
 const tf = require("@tensorflow/tfjs-node");
 async function loadModel() {
   return tf.loadGraphModel(
-    "https://storage.googleapis.com/bucket-latihan-ml/model.json"
+    "https://storage.googleapis.com/bucket-ml-app/model.json"
   );
 }
 module.exports = loadModel;
